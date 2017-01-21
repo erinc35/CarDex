e76f65911190c3525a3fba6182f3da226678845c
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-01-20 17:15:58 -0800

    float-left>div>div margin right and left 2% added

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -4,24 +4,17 @@ $(document).ready(function(){
 
 function tabContent() {
   $('.tabs li').click(function(event){
+
     event.preventDefault();
 
-    // var tab_id = $(this).attr('data-tab');
-    // $("#tab-1").css("display", "block")
-
     $(".tabs").children(".current").removeClass('current');
-    // $(".tab-content").removeClass('current');
     $(this).addClass('current');
-    // console.log($(this))
     var buu = $(this).children("a").attr("href");
     $(".tab-content").css("display", "none");
-    console.log(buu)
     $(buu).css("display", "block");
 
 
-    // $('.tab-content').removeClass('current');
-    // $("#"+tab_id).addClass('current');
-    // $(".tab-content").css("display", "block");
+
      });
   }
 
