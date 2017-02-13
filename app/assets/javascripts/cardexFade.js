9af51a1638fb65b3b5b1a37daa468e1b6ef9c223
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-02-13 07:35:48 -0800

    fade event added to cardex intro

---
+++ app/assets/javascripts/cardexFade.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//   $("#fade").delay(10).animate({ opacity: 1 }, 1000)
+// })
+
+
+// $(window).scroll(function () {
+//     var scrollTop = $(window).scrollTop();
+//     var height = $(window).height();
+//     $('#fade').css({
+//         'opacity': ((height - scrollTop) / height)
+//     });
+// });
+
+
