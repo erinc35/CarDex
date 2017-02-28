90a264285b65c668347b37debcccc45589932a50
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-02-27 22:40:21 -0800

    loginForm event edited

--- app/assets/javascripts/makeModelYearForm.js
+++ app/assets/javascripts/makeModelYearForm.js
@@ -149,7 +149,6 @@ function loginFormPress()  {
   $(".login-link").on("click", function(event){
    event.preventDefault();
    $(".login-form").slideDown();
-
    mouseLeaveBlack();
    mouseEnterWhite();
 
