7e5360f9c7fef996b9ec463fa339d1654d102ad6
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-02-27 22:39:37 -0800

    slideDown event added to login form

--- app/assets/javascripts/makeModelYearForm.js
+++ app/assets/javascripts/makeModelYearForm.js
@@ -149,6 +149,7 @@ function loginFormPress()  {
   $(".login-link").on("click", function(event){
    event.preventDefault();
    $(".login-form").slideDown();
+
    mouseLeaveBlack();
    mouseEnterWhite();
 
